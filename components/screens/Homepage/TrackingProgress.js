95ebfd213cbcd71e3d419c30ed98796b0132c292
fmontallana-integr8 <78574637+fmontallana@users.noreply.github.com>
2025-06-21 08:54:15 +0800

    feat: Refactor status style handling by moving getStatusStyle to utils

--- components/screens/Homepage/TrackingProgress.js
+++ components/screens/Homepage/TrackingProgress.js
@@ -11,21 +11,7 @@ import { Feather, MaterialIcons } from "@expo/vector-icons";
 import { TrackingProgressStyles } from "../../AllStyles/TrackingProgressStyles";
 import { useGetTodayAppointment } from "../../../hooks/useServices.query";
 import AppointmentTrackerCard from "./AppointmentTrackerCard";
-
-export const getStatusStyle = (status) => {
-  switch (status) {
-    case "In Progress":
-      return { backgroundColor: "#E0E7FF", color: "#4338CA" }; // Indigo
-    case "Completed":
-      return { backgroundColor: "#D1FAE5", color: "#065F46" }; // Green
-    case "Booked":
-      return { backgroundColor: "#DBEAFE", color: "#1E40AF" }; // Blue
-    case "Vehicle Arrived":
-      return { backgroundColor: "#FEF3C7", color: "#92400E" }; // Amber
-    default:
-      return { backgroundColor: "#F3F4F6", color: "#4B5563" }; // Gray
-  }
-};
+import { getStatusStyle } from "../../utils/statusStyles";
 
 const steps = [
   {
